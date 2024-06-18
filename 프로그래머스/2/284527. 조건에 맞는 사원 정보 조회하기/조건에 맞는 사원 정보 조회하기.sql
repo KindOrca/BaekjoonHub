4c616fabca5f6d45d720c44b14404ee278c2a6ed
Orca <97031140+KindOrca@users.noreply.github.com>
2024-06-18 13:10:43 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -1,9 +1,7 @@
 select sum(score) SCORE, e.emp_no, e.emp_name, e.position, e.email
 from HR_EMPLOYEES e
-    join HR_DEPARTMENT d on e.dept_id = d.dept_id
     join HR_GRADE g on e.emp_no = g.emp_no
 where year = 2022
 group by e.emp_no
 order by 1 desc
 limit 1
-
