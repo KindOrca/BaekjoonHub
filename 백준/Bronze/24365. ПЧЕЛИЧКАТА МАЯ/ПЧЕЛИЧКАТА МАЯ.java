d8b348d9456a1ff7569fe48b4f6b9b6e88f13b66
Orca <97031140+KindOrca@users.noreply.github.com>
2025-05-07 10:38:04 +0900

    [Bronze IV] Title: ПЧЕЛИЧКАТА МАЯ, Time: 64 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Bronze/24365. ПЧЕЛИЧКАТА МАЯ/ПЧЕЛИЧКАТА МАЯ.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        int mid = (A + B + C) / 3;
+        int ans = (B - mid) + (C - mid) * 2;
+        System.out.println(ans);
+    }
+}
