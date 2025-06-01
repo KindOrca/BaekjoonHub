109d505237e62497838f850823469efb15278806
Orca <97031140+KindOrca@users.noreply.github.com>
2025-06-01 16:53:53 +0900

    [Bronze IV] Title: 호반우가 학교에 지각한 이유 1, Time: 80 ms, Memory: 11508 KB -BaekjoonHub

---
+++ 백준/Bronze/30468. 호반우가 학교에 지각한 이유 1/호반우가 학교에 지각한 이유 1.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        int D = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+        int k = 4 * E - A - B - C - D;
+        System.out.println(Math.max(k, 0));
+    }
+}
