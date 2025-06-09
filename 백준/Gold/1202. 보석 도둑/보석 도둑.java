901002a31e196aac3ccb7f75a4edfb4c059f2e0a
Orca <97031140+KindOrca@users.noreply.github.com>
2025-06-09 14:21:12 +0900

    [Gold II] Title: 보석 도둑, Time: 1368 ms, Memory: 117600 KB -BaekjoonHub

---
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+class Jewel {
+    int m, v;
+    public Jewel(int m, int v) {
+        this.m = m;
+        this.v = v;
+    }
+}
+
+public class Main {
+    static PriorityQueue<Jewel> pqWeight = new PriorityQueue<>((j1, j2) -> j1.m - j2.m);
+    static PriorityQueue<Jewel> pqCost = new PriorityQueue<>((j1, j2) -> j2.v - j1.v);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; ++i) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int M = Integer.parseInt(st.nextToken());
+            int V = Integer.parseInt(st.nextToken());
+            pqWeight.add(new Jewel(M, V));
+        }
+        int[] C = new int[K];
+        for (int i = 0; i < K; ++i) {
+            C[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(C);
+        long ans = 0;
+        for (int i = 0; i < K; ++i) {
+            while (!pqWeight.isEmpty() && pqWeight.peek().m <= C[i]) {
+                pqCost.add(pqWeight.poll());
+            }
+            if (!pqCost.isEmpty()) ans += pqCost.poll().v;
+        }
+        System.out.println(ans);
+    }
+}
