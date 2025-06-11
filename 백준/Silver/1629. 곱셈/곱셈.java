d2df90ae2b992f94fbbf53f4f6dc817c3e60a9e4
Orca <97031140+KindOrca@users.noreply.github.com>
2025-06-11 17:15:10 +0900

    [Silver I] Title: 곱셈, Time: 64 ms, Memory: 11476 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.java
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -5,20 +5,19 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int A = Integer.parseInt(st.nextToken());
-        int B = Integer.parseInt(st.nextToken());
-        int C = Integer.parseInt(st.nextToken());
-        System.out.println(power(A, B, C));
+        long A = Integer.parseInt(st.nextToken());
+        long B = Integer.parseInt(st.nextToken());
+        long C = Integer.parseInt(st.nextToken());
+        System.out.println(multiple(A, B, C));
     }
 
-    static long power(int a, int b, int c) {
+    static long multiple(long a, long b, long c) {
         if (b == 1) return a % c;
-        long temp = power(a, b / 2, c);
-        temp = (temp * temp) % c;
+        long temp = multiple(a, b / 2, c);
         if (b % 2 == 1) {
-            return (temp * (a % c)) % c;
-        } else {
-            return temp;
+            return ((a % c) * (temp * temp % c)) % c;
+        } else{
+            return (temp * temp % c) % c;
         }
     }
 }
