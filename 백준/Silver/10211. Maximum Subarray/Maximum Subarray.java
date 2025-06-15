f8045fd80aebef567cd87720e97b316ef22458de
Orca <97031140+KindOrca@users.noreply.github.com>
2025-06-15 12:09:27 +0900

    [Silver IV] Title: Maximum Subarray, Time: 92 ms, Memory: 12856 KB -BaekjoonHub

--- 백준/Silver/10211. Maximum Subarray/Maximum Subarray.java
+++ 백준/Silver/10211. Maximum Subarray/Maximum Subarray.java
@@ -7,20 +7,18 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
         int T = Integer.parseInt(br.readLine());
-        for (int t = 0; t < T; ++t) {
+        while (T-- > 0) {
             int N = Integer.parseInt(br.readLine());
             int[] A = new int[N];
-            int[] pSum = new int[N + 1];
             st = new StringTokenizer(br.readLine(), " ");
             for (int i = 0; i < N; ++i) {
                 A[i] = Integer.parseInt(st.nextToken());
-                pSum[i + 1] = pSum[i] + A[i];
             }
-            int max = A[0];
+            int max = A[0], pSum1 = 0, pSum2 = 0;
             for (int i = 0; i < N; ++i) {
-                for (int j = i + 1; j < N + 1; ++j) {
-                    max = Math.max(max, pSum[j] - pSum[i]);
-                }
+                pSum1 += A[i];
+                max = Math.max(max, pSum1 - pSum2);
+                pSum2 = Math.min(pSum1, pSum2);
             }
             sb.append(max).append("\n");
         }
