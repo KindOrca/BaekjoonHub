04b67f94c08e53d3779b6f76b590b814230064af
Orca <97031140+KindOrca@users.noreply.github.com>
2025-07-12 19:02:34 +0900

    [Gold IV] Title: 미로만들기, Time: 68 ms, Memory: 11784 KB -BaekjoonHub

---
+++ 백준/Gold/2665. 미로만들기/미로만들기.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+
+class Point {
+    int x, y;
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+public class Main {
+    static int N;
+    static int[][] map;
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {-1, 0, 1, 0};
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        map = new int[N][N];
+        for (int i = 0; i < N; ++i) {
+            String s = br.readLine();
+            for (int j = 0; j < N; ++j) {
+                map[i][j] = s.charAt(j) - '0';
+            }
+        }
+        System.out.println(solve());
+    }
+
+    static int solve() {
+        Deque<Point> deq = new ArrayDeque<>();
+        deq.add(new Point(0, 0));
+        int[][] visited = new int[N][N];
+        for (int i = 0; i < N; ++i) Arrays.fill(visited[i], 2500);
+        visited[0][0] = 0;
+        int nx, ny;
+        while (!deq.isEmpty()) {
+            Point p = deq.poll();
+            if (p.x == N - 1 && p.y == N - 1) continue;
+            for (int i = 0; i < 4; ++i) {
+                nx = p.x + dx[i];
+                ny = p.y + dy[i];
+                if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
+                if (map[ny][nx] == 0) {
+                    if (visited[ny][nx] <= visited[p.y][p.x] + 1) continue;
+                    visited[ny][nx] = visited[p.y][p.x] + 1;
+                    deq.addLast(new Point(nx, ny));
+                } else {
+                    if (visited[ny][nx] <= visited[p.y][p.x]) continue;
+                    visited[ny][nx] = visited[p.y][p.x];
+                    deq.addFirst(new Point(nx, ny));
+                }
+            }
+        }
+        return visited[N - 1][N - 1];
+    }
+}
