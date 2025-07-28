5500fdea9478495d53592d9611b34dfce9f81e64
Orca <97031140+KindOrca@users.noreply.github.com>
2025-07-28 20:58:16 +0900

    [Bronze IV] Title: 코드마스터 2024, Time: 64 ms, Memory: 11440 KB -BaekjoonHub

--- 백준/Bronze/32215. 코드마스터 2024/코드마스터 2024.java
+++ 백준/Bronze/32215. 코드마스터 2024/코드마스터 2024.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int n = Integer.parseInt(st.nextToken());
+        st.nextToken();
         int m = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
         System.out.println(m * k + m);
