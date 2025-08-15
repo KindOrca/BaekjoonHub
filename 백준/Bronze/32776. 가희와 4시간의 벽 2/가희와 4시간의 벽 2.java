7b0b197bf100b7dbfae39dc77c87bbc83d3eb8e5
Orca <97031140+KindOrca@users.noreply.github.com>
2025-08-15 11:58:25 +0900

    [Bronze IV] Title: 가희와 4시간의 벽 2, Time: 64 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Bronze/32776. 가희와 4시간의 벽 2/가희와 4시간의 벽 2.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int S = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        String s;
+        if (S <= a + b + c || S <= 240) s = "high speed rail";
+        else s = "flight";
+        System.out.println(s);
+    }
+}
