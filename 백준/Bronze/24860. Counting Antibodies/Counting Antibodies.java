0871c29d41008e5cc505afceae2ec8db6258da10
Orca <97031140+KindOrca@users.noreply.github.com>
2025-09-17 21:19:27 +0900

    [Bronze IV] Title: Counting Antibodies, Time: 64 ms, Memory: 11476 KB -BaekjoonHub

---
+++ 백준/Bronze/24860. Counting Antibodies/Counting Antibodies.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        long vk = Long.parseLong(st.nextToken());
+        long jk = Long.parseLong(st.nextToken());
+        st = new StringTokenizer(br.readLine(), " ");
+        long vl = Long.parseLong(st.nextToken());
+        long jl = Long.parseLong(st.nextToken());
+        st = new StringTokenizer(br.readLine(), " ");
+        long vh = Long.parseLong(st.nextToken());
+        long dh = Long.parseLong(st.nextToken());
+        long jh = Long.parseLong(st.nextToken());
+        System.out.print(vh * dh * jh * (vk * jk + vl * jl));
+    }
+}
