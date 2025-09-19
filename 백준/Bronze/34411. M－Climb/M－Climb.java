775169ef20c21aac8f60a8c8186fef46354b05aa
Orca <97031140+KindOrca@users.noreply.github.com>
2025-09-19 23:54:00 +0900

    [Bronze V] Title: M-Climb, Time: 64 ms, Memory: 11468 KB -BaekjoonHub

---
+++ 백준/Bronze/34411. M－Climb/M－Climb.java
@@ -0,0 +1,10 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int m = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        br.readLine();
+        System.out.println(n * m);
+    }
+}
