c96653d9e08fa0d9aa31bf461b74666eea36b81e
Orca <97031140+KindOrca@users.noreply.github.com>
2025-09-20 12:08:29 +0900

    [Bronze V] Title: 13 Floors, Time: 64 ms, Memory: 11532 KB -BaekjoonHub

---
+++ 백준/Bronze/34412. 13 Floors/13 Floors.java
@@ -0,0 +1,8 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int x = Integer.parseInt(br.readLine());
+        System.out.println(x > 12 ? x + 1 : x);
+    }
+}
