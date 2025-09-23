8b0e30d419d88e618c720e0eb0c5c7c2fde42bad
Orca <97031140+KindOrca@users.noreply.github.com>
2025-09-23 21:12:02 +0900

    [Bronze IV] Title: Andando no tempo, Time: 68 ms, Memory: 11528 KB -BaekjoonHub

---
+++ 백준/Bronze/13580. Andando no tempo/Andando no tempo.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        System.out.println((a == b || a == c || b == c || a + b == c || a + c == b || b + c == a) ? "S" : "N");
+    }
+}
