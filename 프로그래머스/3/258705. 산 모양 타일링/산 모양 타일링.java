46f5e58d08d74f3fdb6ce76214eb1cca8258cfb0
Orca <97031140+KindOrca@users.noreply.github.com>
2025-10-07 16:08:13 +0900

    [level 3] Title: 산 모양 타일링, Time: 7.92 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
+++ 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
@@ -7,7 +7,7 @@ class Solution {
         dp[1] = tops[0] == 1 ? 4 : 3;
         for (int i = 2; i < n + 1; ++i) {
             int a = (tops[i - 1] == 1 ? 3 * dp[i - 1] : 2 * dp[i - 1]) % MOD;
-            int b = ((dp[i - 1] - dp[i - 2]) % MOD + MOD) % MOD;
+            int b = (dp[i - 1] - dp[i - 2] + MOD) % MOD;
             dp[i] = (a + b) % MOD;
         }
         return dp[n];
