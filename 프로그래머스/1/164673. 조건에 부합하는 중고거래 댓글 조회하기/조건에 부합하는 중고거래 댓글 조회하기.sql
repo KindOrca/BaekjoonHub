f8f424ad1c96d060a1cda54276075e119c339c63
Orca <97031140+KindOrca@users.noreply.github.com>
2025-10-26 09:01:39 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -1,5 +1,6 @@
-select b.title, b.board_id, r.reply_id, r.writer_id, r.contents, date_format(r.created_date, '%Y-%m-%d')
-from used_goods_board b
-    join used_goods_reply r on b.board_id = r.board_id
-where b.created_date regexp '^.*2022-10-.*'
-order by r.created_date, 1
+SELECT B.TITLE, B.BOARD_ID, R.REPLY_ID, R.WRITER_ID, R.CONTENTS, DATE_FORMAT(R.CREATED_DATE, '%Y-%m-%d') AS CREATED_DATE
+FROM USED_GOODS_BOARD AS B
+JOIN USED_GOODS_REPLY AS R ON B.BOARD_ID = R.BOARD_ID
+WHERE YEAR(B.CREATED_DATE) = 2022
+    AND MONTH(B.CREATED_DATE) = 10
+ORDER BY R.CREATED_DATE ASC, B.TITLE ASC;
